eefa8e7aa0a2bbdab95c22187faf7aff926702e9
isadora mello <isadora.mello@gmail.com.br>
2024-05-09 08:41:21 -0300

    Aula final

--- controle_series/resources/views/series/create.blade.php
+++ controle_series/resources/views/series/create.blade.php
@@ -1,9 +1,10 @@
 <x-layout titulo="Nova série">
-    <form action="" method="post">
+    <form action="/series/salvar" method="post">
+        @csrf
         <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
             <input type="text" id="nome" name="nome" class="form-control">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+    </form>
 </x-layout>
--- controle_series/resources/views/series/index.blade.php
+++ controle_series/resources/views/series/index.blade.php
@@ -3,7 +3,7 @@
 <a href="/series/criar" class="btn btn-dark mb-2">Criar Nova serie</a>
     <ul class="list-group">
         <?php foreach ($series as $serie) : ?>
-            <li class="list-group-item"><?php echo $serie; ?></li>
+            <li class="list-group-item"><?php echo $serie->nome; ?></li>
         <?php endforeach; ?>
     </ul>
 </x-layout>
